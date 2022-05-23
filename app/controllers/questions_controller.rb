cb1bb7af409744b6ab16d5a86d84ea2f88bfe9be
diandra elmira <diandraelmira@gmail.com>
2022-05-23 14:06:29 +0100

    done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @questions = params[:question]
+
+    if @questions.include?('?')
+      @answer = ' Silly question, get dressed and go to work!'
+    elsif @questions.capitalize == 'I am going to work'
+      @answer = ' Great!'
+    else
+      @answer = 'I don\'t care, get dressed and go to work!'
+    end
+  end
+end
